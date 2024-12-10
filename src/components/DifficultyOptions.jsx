6913e46886501306aa9cde9053222d54e46152ee
mjgalarza1 <matias.galarza.unq@gmail.com>
2024-12-10 18:30:45 -0300

    Componetizado el selector de dificultad

---
+++ src/components/DifficultyOptions.jsx
@@ -0,0 +1,29 @@
+import {ButtonGroup, ToggleButton} from "react-bootstrap";
+
+const difficulties = [
+    { name: '4x4', value: '4x4' },
+    { name: '6x4', value: '6x4' },
+    { name: '6x6', value: '6x6' },
+];
+
+function DifficultyOptions({ currentDifficultyValue, handleDifficulty }) {
+    return (
+        <ButtonGroup>
+            {difficulties.map((selectedDifficulty, idx) => (
+                <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    name="radio"
+                    value={selectedDifficulty.value}
+                    checked={currentDifficultyValue === selectedDifficulty.value}
+                    onChange={(e) => handleDifficulty(e.currentTarget.value)}
+                >
+                    {selectedDifficulty.name}
+                </ToggleButton>
+            ))}
+        </ButtonGroup>
+    );
+}
+
+export default DifficultyOptions;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,16 @@
 import Button from "../components/Button/Button"
 import { useNavigate } from 'react-router-dom';
-import { ButtonGroup, ToggleButton } from "react-bootstrap";
 import {useState} from "react";
+import DifficultyOptions from "../components/DifficultyOptions.jsx";
 
 function Home() {
     const navigate = useNavigate();
 
     const [difficultyValue, setDifficultyValue] = useState('4x4');
 
-    const difficulties = [
-        { name: '4x4', value: '4x4' },
-        { name: '6x4', value: '6x4' },
-        { name: '6x6', value: '6x6' },
-    ];
+    const handleDifficulty = (optionSelected) => {
+        setDifficultyValue(optionSelected);
+    };
 
     return (
         <>
@@ -21,30 +19,19 @@ function Home() {
                 <p>Un juego de memoria con aves</p>
             </div>
             <div className='main-menu'>
+
                 <div className="d-flex flex-column m-3">
                     <div className="d-flex flex-row justify-content-between align-items-center">
                         <p className="m-0">Dificultad</p>
-                        <ButtonGroup>
-                            {difficulties.map((difficulty, idx) => (
-                                <ToggleButton
-                                    key={idx}
-                                    id={`radio-${idx}`}
-                                    type="radio"
-                                    name="radio"
-                                    value={difficulty.value}
-                                    checked={difficultyValue === difficulty.value}
-                                    onChange={(e) => setDifficultyValue(e.currentTarget.value)}
-                                >
-                                    {difficulty.name}
-                                </ToggleButton>
-                            ))}
-                        </ButtonGroup>
+                        <DifficultyOptions currentDifficultyValue={difficultyValue} handleDifficulty={handleDifficulty} />
                     </div>
                 </div>
+
                 <div className="main-menu-buttons">
                     <Button text={"Comenzar partida"} onClick={() => navigate(`/game/${difficultyValue}`)}/>
                     <Button text={"Reglas de Juego"} onClick={() => navigate('/rules')}/>
                 </div>
+
             </div>
         </>
     );

3aa7ae1104f3dcb501bf740c16eef2f2a77db54d
mjgalarza1 <matias.galarza.unq@gmail.com>
2024-12-10 22:21:47 -0300

    Fix del tamaño del tablero y sus cartas

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,13 +1,14 @@
 import Board from "../../components/Board/Board.jsx";
 import Button from "../../components/Button/Button.jsx";
 import {useNavigate} from "react-router-dom";
+import "./Game.css"
 
 function Game() {
     const navigate = useNavigate();
 
     return (
         <>
-            <div style={{textAlign: "center", display: "flex", flexDirection: "column", gap:"2em"}}>
+            <div className="game-board-container">
                 <h1 style={{margin: 0}}>¡Descubre las aves!</h1>
                 <Board />
                 <Button text='Volver al menú' onClick={() => navigate('/home')} />
